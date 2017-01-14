d6a065c4d8bf602715e3bffc0b039b9e8d5bc80b
AAJM van Montfort <lout@macbook-pro-001-van-aajm-van-montfort.local>
2017-01-14 19:52:36 +0100

    Final version, cleaned up

--- pageScrollTest/ViewController.swift
+++ pageScrollTest/ViewController.swift
@@ -9,10 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var contentSize: CGFloat = 0.0
+        
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x)")
+            let imageView = UIImageView(image: image)
+            
+            var newX: CGFloat = 0.0
+            newX = view.frame.midX + (view.frame.size.width * CGFloat(x))
+            contentSize += newX
+            
+            scrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: newX - 75, y: view.frame.midY - 75, width: 150, height: 150)
+            
+        }
+        
+        scrollView.contentSize = CGSize(width: contentSize, height: view.frame.size.height)
     }
 
     override func didReceiveMemoryWarning() {
