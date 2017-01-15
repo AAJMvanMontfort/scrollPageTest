1f501347fa38cfb84eb15c776b4ebd334a16fb9c
AAJM van Montfort <lout@macbook-pro-001-van-aajm-van-montfort.local>
2017-01-15 09:43:19 +0100

    Small clean up done

--- pageScrollTest/ViewController.swift
+++ pageScrollTest/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIGestureRecognizerDelegate {
+class ViewController: UIViewController {
     
     @IBOutlet weak var scrollView: UIScrollView!
     var images = [UIImageView]()
